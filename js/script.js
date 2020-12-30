fda11bc5c4e76e873ba123e708c3d5be2f78522f
Disney40 <76501862+Disney40@users.noreply.github.com>
2020-12-30 09:00:43 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator - Star Wars Edition
+******************************************/
+
+
+// CREATES ARRAY OF OBJECTS TO HOLD ALL QUOTES
+
